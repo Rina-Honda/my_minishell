7ad6bd565a6237cb105f279a50a7d5a1d20d3493
Rina Honda <rhonda@c5r3s2.42tokyo.jp>
2025-03-18 11:16:44 +0900

    parse norm modified

--- src/parse/parse_simple_command.c
+++ src/parse/parse_simple_command.c
@@ -6,7 +6,7 @@
 /*   By: rhonda <rhonda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 23:32:21 by rhonda            #+#    #+#             */
-/*   Updated: 2025/03/18 00:39:30 by rhonda           ###   ########.fr       */
+/*   Updated: 2025/03/18 11:15:34 by rhonda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	append_redirect_recursive(t_command **redirect, t_command *element)
 	append_redirect_recursive(&(*redirect)->next, element);
 }
 
-void	append_command_element(t_command *command, t_token **rest, t_token *token)
+void	append_command_elem(t_command *command, t_token **rest, t_token *token)
 {
 	if (token->kind == TK_WORD)
 	{
@@ -30,15 +30,19 @@ void	append_command_element(t_command *command, t_token **rest, t_token *token)
 		token = token->next;
 	}
 	else if (equal_operator(token, ">") && token->next->kind == TK_WORD)
-		append_redirect_recursive(&command->redirects, redirect_out(&token, token));
+		append_redirect_recursive(&command->redirects,
+			redirect_out(&token, token));
 	else if (equal_operator(token, "<") && token->next->kind == TK_WORD)
-		append_redirect_recursive(&command->redirects, redirect_in(&token, token));
+		append_redirect_recursive(&command->redirects,
+			redirect_in(&token, token));
 	else if (equal_operator(token, ">>") && token->next->kind == TK_WORD)
-		append_redirect_recursive(&command->redirects, redirect_append(&token, token));
+		append_redirect_recursive(&command->redirects,
+			redirect_append(&token, token));
 	else if (equal_operator(token, "<<") && token->next->kind == TK_WORD)
-		append_redirect_recursive(&command->redirects, redirect_heredoc(&token, token));
+		append_redirect_recursive(&command->redirects,
+			redirect_heredoc(&token, token));
 	else
-		todo("append_command_element");
+		todo("append_command_elem");
 	*rest = token;
 }
 
@@ -63,9 +67,9 @@ t_command	*simple_command(t_token **rest, t_token *token)
 	t_command	*command;
 
 	command = new_command(SIMPLE_CMD);
-	append_command_element(command, &token, token);
+	append_command_elem(command, &token, token);
 	while (token && !at_eof(token) && !is_control_operator(token))
-		append_command_element(command, &token, token);
+		append_command_elem(command, &token, token);
 	*rest = token;
 	return (command);
 }
