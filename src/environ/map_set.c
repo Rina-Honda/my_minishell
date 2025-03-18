ec67d6f9f064c31e210b3f6305e36c0c5a3aa7c8
Rina Honda <rhonda@c5r3s2.42tokyo.jp>
2025-03-18 10:03:14 +0900

    map_set diveded

--- src/environ/map_set.c
+++ src/environ/map_set.c
@@ -6,11 +6,44 @@
 /*   By: rhonda <rhonda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 22:52:27 by rhonda            #+#    #+#             */
-/*   Updated: 2025/03/17 22:53:04 by rhonda           ###   ########.fr       */
+/*   Updated: 2025/03/18 10:01:26 by rhonda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+static void	map_item_update(t_item *item, const char *value)
+{
+	free(item->value);
+	if (!value)
+		item->value = NULL;
+	else
+	{
+		item->value = ft_strdup(value);
+		if (!item->value)
+			fatal_error("map_set ft_strdup");
+	}
+}
+
+static void	map_insert(t_map *map, const char *name, const char *value)
+{
+	t_item	*item;
+
+	if (!value)
+	{
+		item = item_new(ft_strdup(name), NULL);
+		if (!item->name)
+			fatal_error("strdup");
+	}
+	else
+	{
+		item = item_new(ft_strdup(name), ft_strdup(value));
+		if (!item->name || !item->value)
+			fatal_error("ft_strdup");
+	}
+	item->next = map->item_head.next;
+	map->item_head.next = item;
+}
 
 int	map_set(t_map *map, const char *name, const char *value)
 {
@@ -18,41 +51,10 @@ int	map_set(t_map *map, const char *name, const char *value)
 
 	if (!name || !is_identifier(name))
 		return (-1);
-	current = map->item_head.next;
-	while (current)
-	{
-		if (ft_strcmp(current->name, name) == 0)
-			break ;
-		current = current->next;
-	}
+	current = map_get(map, name);
 	if (current)
-	{
-		free(current->value);
-		if (!value)
-			current->value = NULL;
-		else
-		{
-			current->value = ft_strdup(value);
-			if (!current->value)
-				fatal_error("ft_strdup");
-		}
-	}
+		map_item_update(current, value);
 	else
-	{
-		if (!value)
-		{
-			current = item_new(ft_strdup(name), NULL);
-			if (!current->name)
-				fatal_error("strdup");
-		}
-		else
-		{
-			current = item_new(ft_strdup(name), ft_strdup(value));
-			if (!current->name || !current->value)
-				fatal_error("ft_strdup");
-		}
-		current->next = map->item_head.next;
-		map->item_head.next = current;
-	}
+		map_insert(map, name, value);
 	return (0);
 }
